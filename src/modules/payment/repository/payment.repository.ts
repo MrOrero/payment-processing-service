eb069572bbd5f555a936ab03abb5734cf867b116
Orero Ozore <oreroozore@gmail.com>
2025-12-12 11:48:11 +0100

    implemented payment repository

---
+++ src/modules/payment/repository/payment.repository.ts
@@ -0,0 +1,25 @@
+import { Payment } from "../entity/payment.entity";
+import { PaymentStatus } from "../../../libs/constants";
+
+export class PaymentRepository {
+    private payments: Payment[] = [];
+
+    create(payment: Payment): Payment {
+        this.payments.push(payment);
+        return payment;
+    }
+
+    findById(id: string): Payment | null {
+        return this.payments.find(payment => payment.id === id) || null;
+    }
+
+    findOneAndUpdate(id: string, status: PaymentStatus, failureReason?: string): Payment | null {
+        const payment = this.findById(id);
+        if (!payment) {
+            return null;
+        }
+        
+        payment.updateStatus(status, failureReason);
+        return payment;
+    }
+}
